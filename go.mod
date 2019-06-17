3105d4ba6f785d3355f481e106f380e523fee555
Yung Trinh <ytrinh@gmail.com>
2019-06-17 15:29:11 -0500

    Add grpc_dial

--- go.mod
+++ go.mod
@@ -4,10 +4,10 @@ go 1.12
 
 require (
 	github.com/gogo/protobuf v1.2.1 // indirect
-	github.com/golang/protobuf v1.3.1 // indirect
 	github.com/grpc-ecosystem/go-grpc-middleware v1.0.1-0.20190603140646-27f3801344b2
+	github.com/grpc-ecosystem/go-grpc-prometheus v1.2.0
+	github.com/prometheus/client_golang v1.0.0 // indirect
 	github.com/sirupsen/logrus v1.4.2
-	github.com/stretchr/testify v1.3.0 // indirect
 	golang.org/x/net v0.0.0-20190606173856-1492cefac77f // indirect
 	google.golang.org/genproto v0.0.0-20180831171423-11092d34479b // indirect
 	google.golang.org/grpc v1.21.1
---
+++ grpc_dial.go
@@ -0,0 +1,28 @@
+package ms
+
+import (
+	grpc_middleware "github.com/grpc-ecosystem/go-grpc-middleware"
+	grpc_logrus "github.com/grpc-ecosystem/go-grpc-middleware/logging/logrus"
+	grpc_prometheus "github.com/grpc-ecosystem/go-grpc-prometheus"
+	"github.com/sirupsen/logrus"
+	"google.golang.org/grpc"
+)
+
+// GRPCDial is a helper function that creates a grpc.ClientConn with
+// logging and prometheus interecepters
+func GRPCDial(url string, entry *logrus.Entry) (*grpc.ClientConn, error) {
+	opts := []grpc_logrus.Option{
+		grpc_logrus.WithLevels(grpc_logrus.DefaultClientCodeToLevel),
+	}
+
+	return grpc.Dial(
+		url,
+		grpc.WithInsecure(),
+		grpc.WithUnaryInterceptor(
+			grpc_middleware.ChainUnaryClient(
+				grpc_logrus.UnaryClientInterceptor(entry, opts...),
+				grpc_prometheus.UnaryClientInterceptor,
+			),
+		),
+	)
+}
